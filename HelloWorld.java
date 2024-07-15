6c9fe86592bce3c77831ecb04e4533ae4cb0423b
ednuetech <166903715+ednuetech@users.noreply.github.com>
2024-07-15 18:22:12 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld { //Defines a class HelloWorld
+          public static void main (String[] args){ 
+                    // public again means that anyone can access it.
+                    // static means that you can run this method without creating an instance of Main.
+                    // void means that this method doesn't return any value.
+                    // main is the name of the method.
+                    System.out.println("Hello world");
+          }
+}
